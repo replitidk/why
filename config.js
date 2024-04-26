efb848a62662a0c28c968ecb48cc1e3e71be74ed
replitidk <150942685+replitidk@users.noreply.github.com>
2024-04-26 10:06:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    admindanke: "6895",
+    admindanke: '6895', ryan: '1391', bishop: '3912', adminvince: '1937', fortnitekid997: '9102' , royalxcypress: '4592' , guest: '1242'
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
